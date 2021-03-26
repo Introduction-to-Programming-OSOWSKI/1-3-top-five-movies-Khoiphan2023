343f884719b6a3044161f89bfd1c09b4f534ca58
Khoiphan2023 <820929@mystma.org>
2021-03-26 14:35:13 +0000

    TopFiveMovies

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-#WRITE YOUR CODE IN THIS FILE
+def TopFiveMovies(A, B, C, D, E,):
+    return "A, B, C, D, E,"
+print("Star Wars The Empire Strike Back", "Alita Battle Angel", "Joker", "Star Wars Revenge of the Sith", "Transformer")
